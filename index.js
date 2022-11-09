7fad5453f807f3478d9237b150f8a4084c56c8bb
hovhannes.babayan <hovhannes.babayan@fouraitch.com>
2022-11-09 14:14:56 +0400

    add minio support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Package = require("./package.json");
 
 var AWS = require("aws-sdk"),
+	Minio = require("minio"),
 	mime = require("mime"),
 	uuid = require("uuid").v4,
 	fs = require("fs"),
@@ -18,20 +19,54 @@ var plugin = {};
 
 "use strict";
 
-var S3Conn = null;
-var settings = {
-	"accessKeyId": process.env.AWS_ACCESS_KEY_ID ,
-	"secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY ,
-	"region": process.env.AWS_DEFAULT_REGION || "us-east-1",
-	"bucket": process.env.S3_UPLOADS_BUCKET || undefined,
-	"host": process.env.S3_UPLOADS_HOST || "s3.amazonaws.com",
-	"path": process.env.S3_UPLOADS_PATH || undefined,
-	"acl" : process.env.ACL || undefined,
-	"cloudFrontDomain": process.env.CLOUD_FRONT_DOMAIN || undefined,
-};
+var connection = null;
+var uploadProvider = process.env.UPLOAD_PROVIDER || "s3";
+
+var settings = initSettingsFromEnv();
 
 var accessKeyIdFromDb = false;
 var secretAccessKeyFromDb = false;
+var serviceAccountFromDb = false;
+
+function initSettingsFromEnv() {
+	switch(uploadProvider) {
+		case "s3": {
+			return {
+				"provider": "s3",
+				"accessKeyId": process.env.AWS_ACCESS_KEY_ID ,
+				"secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY ,
+				"region": process.env.AWS_DEFAULT_REGION || "us-east-1",
+				"bucket": process.env.S3_UPLOAD_BUCKET || undefined,
+				"host": process.env.S3_UPLOAD_HOST || "s3.amazonaws.com",
+				"path": process.env.S3_UPLOAD_PATH || undefined,
+				"acl" : process.env.ACL || undefined,
+				"cloudFrontDomain": process.env.CLOUD_FRONT_DOMAIN || undefined,
+			}
+		}
+		case "gcs": {
+			return {
+				"provider": "gcs",
+				"serviceAccount": JSON.parse(Buffer.from(process.env.GCS_SERVICE_ACCOUNT || '', 'base64').toString('utf-8') || '{}'),
+				"bucket": process.env.GCS_UPLOAD_BUCKET || undefined,
+				"host": process.env.GCS_UPLOAD_HOST || "storage.googleapis.com",
+				"path": process.env.GCS_UPLOAD_PATH || undefined,
+			}
+		}
+		case "minio": {
+			return {
+				"provider": "minio",
+				"accessKeyId": process.env.MINIO_ACCESS_KEY ,
+				"secretAccessKey": process.env.MINIO_SECRET_KEY ,
+				"bucket": process.env.MINIO_UPLOAD_BUCKET || undefined,
+				"host": process.env.MINIO_UPLOAD_HOST || "localhost:9000",
+				"path": process.env.MINIO_UPLOAD_PATH || undefined,
+			}
+		}
+		default: {
+			return {};
+		}
+	} 
+}
 
 function fetchSettings(callback) {
 	db.getObjectFields(Package.name, Object.keys(settings), function (err, newSettings) {
@@ -43,46 +78,116 @@ function fetchSettings(callback) {
 			return;
 		}
 
-		accessKeyIdFromDb = false;
-		secretAccessKeyFromDb = false;
-
-		if (newSettings.accessKeyId) {
-			settings.accessKeyId = newSettings.accessKeyId;
-			accessKeyIdFromDb = true;
+		if(!newSettings.provider) {
+			settings.provider = process.env.UPLOAD_PROVIDER || "s3";
 		}
 
-		if (newSettings.secretAccessKey) {
-			settings.secretAccessKey = newSettings.secretAccessKey;
+		if(settings.provider === "s3") {
+			accessKeyIdFromDb = false;
 			secretAccessKeyFromDb = false;
+	
+			if (newSettings.accessKeyId) {
+				settings.accessKeyId = newSettings.accessKeyId;
+				accessKeyIdFromDb = true;
+			}
+	
+			if (newSettings.secretAccessKey) {
+				settings.secretAccessKey = newSettings.secretAccessKey;
+				secretAccessKeyFromDb = false;
+			}
+	
+			if (!newSettings.bucket) {
+				settings.bucket = process.env.S3_UPLOAD_BUCKET || "";
+			}
+	
+			if (!newSettings.host) {
+				settings.host = process.env.S3_UPLOAD_HOST || "";
+			}
+	
+			if (!newSettings.path) {
+				settings.path = process.env.S3_UPLOAD_PATH || "";
+			}
+	
+			if (!newSettings.region) {
+				settings.region = process.env.AWS_DEFAULT_REGION || "";
+			}
+	
+			if (settings.accessKeyId && settings.secretAccessKey) {
+				AWS.config.update({
+					accessKeyId: settings.accessKeyId,
+					secretAccessKey: settings.secretAccessKey
+				});
+			}
+	
+			if (settings.region) {
+				AWS.config.update({
+					region: settings.region
+				});
+			}
+		} 
+
+		if(settings.provider === "minio") {
+			accessKeyIdFromDb = false;
+			secretAccessKeyFromDb = false;
+	
+			if (newSettings.accessKeyId) {
+				settings.accessKeyId = newSettings.accessKeyId;
+				accessKeyIdFromDb = true;
+			}
+	
+			if (newSettings.secretAccessKey) {
+				settings.secretAccessKey = newSettings.secretAccessKey;
+				secretAccessKeyFromDb = false;
+			}
+	
+			if (!newSettings.bucket) {
+				settings.bucket = process.env.MINIO_UPLOAD_BUCKET || "";
+			}
+	
+			if (!newSettings.host) {
+				settings.host = process.env.MINIO_UPLOAD_HOST || "";
+			}
+	
+			if (!newSettings.path) {
+				settings.path = process.env.MINIO_UPLOAD_PATH || "";
+			}
+	
+			if (settings.accessKeyId && settings.secretAccessKey) {
+
+				var urlObj = new URL(!settings.host.startsWith("http") ? "http://" + settings.host : settings.host);
+				connection = new Minio.Client({
+					useSSL: urlObj.protocol === "https:",
+					accessKey: settings.accessKeyId,
+					secretKey: settings.secretKey,
+					endPoint: urlObj.hostname,
+					port: urlObj.port ? parseInt(urlObj.port) : 80,
+				});
+			}
 		}
 
-		if (!newSettings.bucket) {
-			settings.bucket = process.env.S3_UPLOADS_BUCKET || "";
-		}
-
-		if (!newSettings.host) {
-			settings.host = process.env.S3_UPLOADS_HOST || "";
-		}
-
-		if (!newSettings.path) {
-			settings.path = process.env.S3_UPLOADS_PATH || "";
-		}
-
-		if (!newSettings.region) {
-			settings.region = process.env.AWS_DEFAULT_REGION || "";
-		}
-
-		if (settings.accessKeyId && settings.secretAccessKey) {
-			AWS.config.update({
-				accessKeyId: settings.accessKeyId,
-				secretAccessKey: settings.secretAccessKey
-			});
-		}
-
-		if (settings.region) {
-			AWS.config.update({
-				region: settings.region
-			});
+		if(settings.provider === "gcp") {
+			secretAccessKeyFromDb = false;
+	
+			if (newSettings.serviceAccount) {
+				settings.serviceAccount = newSettings.serviceAccount;
+				secretAccessKeyFromDb = true;
+			}
+	
+			if (!newSettings.bucket) {
+				settings.bucket = process.env.GCS_UPLOAD_BUCKET || "";
+			}
+	
+			if (!newSettings.host) {
+				settings.host = process.env.GCS_UPLOAD_HOST || "";
+			}
+	
+			if (!newSettings.path) {
+				settings.path = process.env.GCS_UPLOAD_PATH || "";
+			}
+	
+			if (settings.serviceAccount) {
+				// TODO
+			}
 		}
 
 		if (typeof callback === "function") {
@@ -91,12 +196,31 @@ function fetchSettings(callback) {
 	});
 }
 
-function S3() {
-	if (!S3Conn) {
-		S3Conn = new AWS.S3();
+function getConnection() {
+
+	if (!connection) {
+
+		if(settings.provider === "s3") {
+			connection = new AWS.S3();
+		}
+
+		if(settings.provider === "minio") {
+			var urlObj = new URL(!settings.host.startsWith("http") ? "http://" + settings.host : settings.host);
+			connection = new Minio.Client({
+				useSSL: urlObj.protocol === "https:",
+				accessKey: settings.accessKeyId,
+				secretKey: settings.secretKey,
+				endPoint: urlObj.hostname,
+				port: urlObj.port ? parseInt(urlObj.port) : 80,
+			});
+		}
+
+		if(settings.provider === "gcs") {
+			// TODO
+		}
 	}
 
-	return S3Conn;
+	return connection;
 }
 
 function makeError(err) {
@@ -111,15 +235,15 @@ function makeError(err) {
 }
 
 plugin.activate = function (data) {
-	if (data.id === 'nodebb-plugin-s3-uploads') {
+	if (data.id === 'nodebb-plugin-upload') {
 		fetchSettings();
 	}
 
 };
 
 plugin.deactivate = function (data) {
-	if (data.id === 'nodebb-plugin-s3-uploads') {
-		S3Conn = null;
+	if (data.id === 'nodebb-plugin-upload') {
+		connection = null;
 	}
 };
 
@@ -128,13 +252,13 @@ plugin.load = function (params, callback) {
 		if (err) {
 			return winston.error(err.message);
 		}
-		var adminRoute = "/admin/plugins/s3-uploads";
+		var adminRoute = "/admin/plugins/upload";
 
 		params.router.get(adminRoute, params.middleware.applyCSRF, params.middleware.admin.buildHeader, renderAdmin);
 		params.router.get("/api" + adminRoute, params.middleware.applyCSRF, renderAdmin);
 
-		params.router.post("/api" + adminRoute + "/s3settings", s3settings);
-		params.router.post("/api" + adminRoute + "/credentials", credentials);
+		params.router.post("/api" + adminRoute + "/uploadsettings", prepareSettings);
+		params.router.post("/api" + adminRoute + "/uploadcredentials", prepareCredentials);
 
 		callback();
 	});
@@ -149,6 +273,7 @@ function renderAdmin(req, res) {
 		forumPath = forumPath + "/";
 	}
 	var data = {
+		provider: settings.provider,
 		bucket: settings.bucket,
 		host: settings.host,
 		path: settings.path,
@@ -156,15 +281,17 @@ function renderAdmin(req, res) {
 		region: settings.region,
 		accessKeyId: (accessKeyIdFromDb && settings.accessKeyId) || "",
 		secretAccessKey: (accessKeyIdFromDb && settings.secretAccessKey) || "",
+		serviceAccount: (serviceAccountFromDb && settings.serviceAccount) || "",
 		csrf: token
 	};
 
-	res.render("admin/plugins/s3-uploads", data);
+	res.render("admin/plugins/upload", data);
 }
 
-function s3settings(req, res, next) {
+function prepareSettings(req, res, next) {
 	var data = req.body;
 	var newSettings = {
+		provider: data.provider || "s3",
 		bucket: data.bucket || "",
 		host: data.host || "",
 		path: data.path || "",
@@ -174,11 +301,12 @@ function s3settings(req, res, next) {
 	saveSettings(newSettings, res, next);
 }
 
-function credentials(req, res, next) {
+function prepareCredentials(req, res, next) {
 	var data = req.body;
 	var newSettings = {
 		accessKeyId: data.accessKeyId || "",
-		secretAccessKey: data.secretAccessKey || ""
+		secretAccessKey: data.secretAccessKey || "",
+		serviceAccount: data.serviceAccount || "",
 	};
 
 	saveSettings(newSettings, res, next);
@@ -222,7 +350,7 @@ plugin.uploadImage = function (data, callback) {
 		}
 
 		fs.readFile(image.path, function (err, buffer) {
-			uploadToS3(image.name, err, buffer, callback);
+			upload(image.name, err, buffer, callback);
 		});
 	}
 	else {
@@ -248,7 +376,7 @@ plugin.uploadImage = function (data, callback) {
 					buf = Buffer.concat([buf, d]);
 				});
 				stdout.on("end", function () {
-					uploadToS3(filename, null, buf, callback);
+					upload(filename, null, buf, callback);
 				});
 			});
 	}
@@ -272,9 +400,21 @@ plugin.uploadFile = function (data, callback) {
 	}
 
 	fs.readFile(file.path, function (err, buffer) {
-		uploadToS3(file.name, err, buffer, callback);
+		upload(file.name, err, buffer, callback);
 	});
 };
+
+function upload(filename, err, buffer, callback) {
+	if(settings.provider === "s3") {
+		uploadToS3(filename, err, buffer, callback);
+	}
+	if(settings.provider === "minio") {
+		uploadToMinio(filename, err, buffer, callback);
+	}
+	if(settings.provider === "gcs") {
+		uploadToGCS(filename, err, buffer, callback);
+	}
+}
 
 function uploadToS3(filename, err, buffer, callback) {
 	if (err) {
@@ -308,7 +448,7 @@ function uploadToS3(filename, err, buffer, callback) {
 		set(params, "ACL", settings.acl);
 	}
 
-	S3().putObject(params, function (err) {
+	getConnection().putObject(params, function (err) {
 		if (err) {
 			return callback(makeError(err));
 		}
@@ -332,13 +472,60 @@ function uploadToS3(filename, err, buffer, callback) {
 	});
 }
 
+function uploadToMinio(filename, err, buffer, callback) {
+	if (err) {
+		return callback(makeError(err));
+	}
+
+	var minioPath;
+	if (settings.path && 0 < settings.path.length) {
+		minioPath = settings.path;
+
+		if (!minioPath.match(/\/$/)) {
+			// Add trailing slash
+			minioPath = minioPath + "/";
+		}
+	}
+	else {
+		minioPath = "/";
+	}
+
+	var minioKeyPath = minioPath.replace(/^\//, ""); // S3 Key Path should not start with slash.
+	var newFilename = minioKeyPath + uuid() + path.extname(filename);
+
+	getConnection().putObject(settings.bucket, newFilename, buffer, function (err) {
+		if (err) {
+			return callback(makeError(err));
+		}
+
+		// amazon has https enabled, we use it by default
+		var host = "https://" + settings.bucket +".play.min.io";
+		if (settings.host && 0 < settings.host.length) {
+			host = settings.host + "/" + settings.bucket;
+			// host must start with http or https
+			if (!host.startsWith("http")) {
+				host = "http://" + host;
+			}
+		}
+
+		callback(null, {
+			name: filename,
+			url: host + "/" + newFilename
+		});
+	});
+}
+
+function uploadToGCS(filename, err, buffer, callback) {
+	// TODO
+}
+
 var admin = plugin.admin = {};
 
 admin.menu = function (custom_header, callback) {
 	custom_header.plugins.push({
-		"route": "/plugins/s3-uploads",
+		"route": "/plugins/upload",
 		"icon": "fa-envelope-o",
-		"name": "S3 Uploads"
+		"name": "Cloud upload"
 	});
 
 	callback(null, custom_header);
---
+++ templates/admin/plugins/upload.tpl
@@ -0,0 +1,164 @@
+<h1><i class="fa fa-picture-o"></i>Upload Configuration</h1>
+<hr/>
+
+<p>You can configure this plugin via <em>environment variables</em>. You can also specify values in the form below, and those will be
+	stored in the database.</p>
+
+<h3>Environment Variables</h3>
+<pre><code>
+// s3
+export AWS_ACCESS_KEY_ID="your-access-key"
+export AWS_SECRET_ACCESS_KEY="your-secret-key"
+export S3_UPLOAD_BUCKET="your-bucket-name"
+export S3_UPLOAD_HOST="host"
+export S3_UPLOAD_PATH="path"
+
+// gcs
+export GCS_SERVICE_ACCOUNT="your-service-account-json"
+export GCS_UPLOAD_BUCKET="your-bucket-name"
+export GCS_UPLOAD_HOST="host"
+export GCS_UPLOAD_PATH="path"
+
+// minio
+export MINIO_ACCESS_KEY="your-access-key"
+export MINIO_SECRET_KEY="your-secret-key"
+export MINIO_UPLOAD_BUCKET="your-bucket-name"
+export MINIO_UPLOAD_HOST="host"
+export MINIO_UPLOAD_PATH="path"
+
+</code></pre>
+
+<p>
+	Asset host and asset path are optional. You can leave these blank to default to the standard asset url -
+	http://mybucket.s3.amazonaws.com/uuid.jpg.<br/>
+	Asset host can be set to a custom asset host. For example, if set to cdn.mywebsite.com then the asset url is
+	http://cdn.mywebsite.com/uuid.jpg.<br/>
+	Asset path can be set to a custom asset path. For example, if set to /assets, then the asset url is
+	http://mybucket.s3.amazonaws.com/assets/uuid.jpg.<br/>
+	If both are asset host and path are set, then the url will be http://cdn.mywebsite.com/assets/uuid.jpg.
+</p>
+
+<h3>Database Stored configuration:</h3>
+<form id="upload-settings">
+	<label for="upload-provider">Upload Provider</label><br/>
+	<select id="upload-provider" name="provider" title="Upload Provider" class="form-control">
+		<option value="s3">AWS S3</option>
+		<option value="gcs">Google Cloud Storage</option>
+		<option value="minio">Minio</option>
+	</select>
+	<label for="bucket">Bucket</label><br/>
+	<input type="text" id="bucket" name="bucket" value="{bucket}" title="Bucket" class="form-control input-lg"
+	       placeholder="Bucket"><br/>
+
+	<label for="host">Host</label><br/>
+	<input type="text" id="host" name="host" value="{host}" title="Host" class="form-control input-lg"
+	       placeholder="website.com or https://website.com"><br/>
+
+	<label for="path">Path</label><br/>
+	<input type="text" id="path" name="path" value="{path}" title="Path" class="form-control input-lg"
+	       placeholder="/assets"><br/>
+
+	<div id="region-wrapper">
+		<label for="region">Region</label><br/>
+		<select id="region" name="region" title="AWS Region" class="form-control">
+			<option value="">..</option>
+			<option value="us-east-1">Standard (us-east-1)</option>
+			<option value="us-west-1">N. California (us-west-1)</option>
+			<option value="us-west-2">Oregon (us-west-2)</option>
+			<option value="ca-central-1">Canada (ca-central-1)</option>
+			<option value="eu-west-1">Ireland (eu-west-1)</option>
+			<option value="eu-west-2">London (eu-west-2)</option>
+			<option value="eu-central-1">Frankfurt (eu-central-1)</option>
+			<option value="ap-northeast-1">Tokyo (ap-northeast-1)</option>
+			<option value="ap-northeast-2">Seoul (ap-northeast-2)</option>
+			<option value="ap-southeast-1">Singapore (ap-southeast-1)</option>
+			<option value="ap-southeast-2">Sydney (ap-southeast-2)</option>
+			<option value="ap-south-1">Mumbai (ap-south-1)</option>
+			<option value="sa-east-1">São Paulo (sa-east-1)</option>
+		</select>
+	</div>
+	<br/>
+
+	<button class="btn btn-primary" type="submit">Save</button>
+</form>
+
+<br><br>
+<form id="upload-credentials">
+	<label for="bucket">Credentials</label><br/>
+	<div class="alert alert-warning">
+		Configuring this plugin using the fields below is <strong>NOT recommended</strong>, as it can be a potential
+		security issue. We highly recommend to use <strong>Environment Variables</strong> instead
+	</div>
+	<div id="credentials">
+		<input type="text" name="accessKeyId" value="{accessKeyId}" maxlength="20" title="Access Key ID"
+			class="form-control input-lg" placeholder="Access Key ID"><br/>
+		<input type="text" name="secretAccessKey" value="{secretAccessKey}" title="Secret Access Key"
+			class="form-control input-lg" placeholder="Secret Access Key"><br/>
+	</div>
+	<div id="service-account" style="display:none">
+		<input type="text" name="serviceAccount" value="{serviceAccount}" title="Service Account JSON (base64 encoded)"
+			class="form-control input-lg" placeholder="Service Account (base64 encoded JSON)"><br/>
+	</div>
+	<button class="btn btn-primary" type="submit">Save</button>
+</form>
+
+<script>
+	$(document).ready(function () {
+
+		$('#region option[value="{region}"]').prop('selected', true)
+		$('#upload-provider option[value="{provider}"]').prop('selected', true);
+
+		$("#upload-settings").on("submit", function (e) {
+			e.preventDefault();
+			save("uploadsettings", this);
+		});
+
+		$("#upload-credentials").on("submit", function (e) {
+			e.preventDefault();
+			var form = this;
+			bootbox.confirm("Are you sure you wish to store your credentials in the database?", function (confirm) {
+				if (confirm) {
+					save("uploadcredentials", form);
+				}
+			});
+		});
+
+		$("#upload-provider").on("change", function (e) {
+			if($(this).val() === "gcs") {
+				$("#credentials").hide();
+				$("#service-account").show();
+			} else {
+				$("#credentials").show();
+				$("#service-account").hide();
+			}
+
+			if($(this).val() === "gcs" || $(this).val() === "minio") {
+				$('#region-wrapper').hide();
+			} else {
+				$('#region-wrapper').show();
+			}
+		})
+		$('#upload-provider').change();
+
+		function save(type, form) {
+			var data = {
+				_csrf: '{csrf}' || $('#csrf_token').val()
+			};
+
+			var values = $(form).serializeArray();
+			for (var i = 0, l = values.length; i < l; i++) {
+				data[values[i].name] = values[i].value;
+			}
+
+			$.post('{forumPath}api/admin/plugins/upload/' + type, data).done(function (response) {
+				if (response) {
+					ajaxify.refresh();
+					app.alertSuccess(response);
+				}
+			}).fail(function (jqXHR, textStatus, errorThrown) {
+				ajaxify.refresh();
+				app.alertError(jqXHR.responseJSON ? jqXHR.responseJSON.error : 'Error saving!');
+			});
+		}
+	});
+</script>
